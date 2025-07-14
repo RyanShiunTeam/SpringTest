bfa2269cd6da69f42507daf01156732657244e8b
ryan-shiun <a0976828118@gmail.com>
2025-07-14 15:44:32 +0800

    test5

--- src/main/java/com/example/ioc/MyController.java
+++ src/main/java/com/example/ioc/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class MyController { // Servlet
 
     @Autowired
     @Qualifier("hpPrinter")
@@ -18,3 +18,4 @@ public class MyController {
         return "Hello World";
     }
 }
+// http://localhost:8080/Bank/queryActivity
