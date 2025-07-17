1832718942eb08454b934d8820b7e53fc9009ce9
TINA6833 <wuyating129@gmail.com>
2025-07-17 18:47:12 +0800

    port練習

--- src/main/java/com/example/ioc/HpPrinter.java
+++ src/main/java/com/example/ioc/HpPrinter.java
@@ -4,7 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-//
+///
 @Component
 public class HpPrinter implements Printer {
 
